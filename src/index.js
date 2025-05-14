74924575ab05fdab87561a346cd49b80d53fef7a
Harsh Tiwari <harshtiwari343@email.com>
2025-05-14 11:14:56 +0530

    added with and without JSX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const flowers = ["Rose", "Dahlia", "Magnolia", "Tulip", "Daisy"];
+
+// Without JSX 
+const element1 = React.createElement("h1", {}, "Programming with Harsh");
+const element2 = React.createElement("ul", {},
+  flowers.map((item, index) => React.createElement("li", { key: index }, item))
+)
+
+// With JSX
+const element3 = <h1>Programming with Harsh 2 + 2 is {2 + 2}</h1>
+const element4 = <ul>
+  {flowers.map((item, index) => (
+    <li key={index}>{item}</li>
+  ))}
+</ul>
+
 root.render(
-  "Programming with Harsh"
+  element4
 )
