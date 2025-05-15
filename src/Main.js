4cba36f8d9d0c9c10b93971d15f6421f606ca49f
Harsh Tiwari <harshtiwari343@email.com>
2025-05-15 11:32:38 +0530

    Added Styling

--- src/Main.js
+++ src/Main.js
@@ -10,35 +10,20 @@ export default class Main extends React.Component { // In ES6 Exporting a module
     constructor(props) { // lifecycle
         super(props);
         // console.log(this.props);
-        this.state = {
-            username: "Surabhi",
-            address: "Jaipur"
-        }
         console.log("Constructor lifecycle");
-    }
-
-    updateDetails = () => {
-        this.setState({ // Whenever state update, component rerender
-            username: "Surya",
-            address: "Bhubneshwar",
-            pin: "212121"
-        })
     }
 
     render() { // render lifecycle
         console.log("Render lifecycle");
         console.log(this.state);
+
         return <div>
+
             <h1>This is Main Class Component</h1>
-            <div>State Username is {this.state.username} and address is {this.state.address}</div>
-            <button onClick={this.updateDetails}>Update State Details</button>
-            <div>Props x is {this.props.x}</div>
-            <div>Props username is {this.props.usernameProps}</div>
-            <div>Props interests is {this.props.interestsProps[0]}</div>
-            <div>Props birth place is {this.props.birthProps.place}</div>
+
             <ProductList {...this.props} />
             <AddProduct data={this.state} />
-            <Welcome message="Welcome to Dashboard" />
+            <Welcome message="Welcome to Dashboard" {...this.props} />
             <Title title="This is Title" />
             <Star />
         </div>
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import './Welcome.css';
+
 class Welcome extends React.Component {
     constructor(props) {
         super(props);
         // console.log("Welcome component", this.props);
         console.log("Welcome component constructor");
+        this.state = {
+            username: "Surabhi",
+            address: "Jaipur"
+        }
+    }
+
+    updateDetails = () => {
+        this.setState({ // Whenever state update, component rerender
+            username: "Surya",
+            address: "Bhubneshwar",
+            pin: "212121"
+        })
     }
 
     render() {
+        const myStyle = {
+            color: 'white',
+            backgroundColor: 'blue',
+            padding: '20px',
+            fontFamily: 'Sans-Serif'
+        }
+
         return <div>
-            <h1>This is Welcome Class Component</h1><h2>Another element</h2>
+            <h1 className='header'>Programming with Harsh</h1>
+            <h1 style={myStyle}>This is Welcome Class Component</h1><h2 className='header'>Another element</h2>
             <div>Props message is {this.props.message}</div>
+
+            <div style={myStyle}>
+                <div>State Username is {this.state.username} and address is {this.state.address}</div>
+                <button onClick={this.updateDetails}>Update State Details</button>
+            </div>
+            <div style={{ color: 'blue', border: '2px solid red', textAlign: 'center', padding: "2px" }}>
+                <div>Props x is {this.props.x}</div>
+                <div>Props username is {this.props.usernameProps}</div>
+                <div>Props interests is {this.props.interestsProps[0]}</div>
+                <div>Props birth place is {this.props.birthProps.place}</div>
+            </div>
         </div>
     }
 }
